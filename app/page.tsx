95a6697d73bc5419692c4d34567de24e8521cb17
Mohammed <mtajir903@gmail.com>
2025-11-11 12:46:19 +0530

    Menu is done with responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,80 @@
-import Image from "next/image";
+"use client";
+import Image from 'next/image';
+import BackgroundImage from '../images/Background.jpg';
+import Menu from '../images/Menu.png';
+import Logo from '../images/logo.png';
+import BackgroundImageDesktop from '../images/BackgroundDesktopVersion.png';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isMenu , SetIsMenu] = useState(true);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="w-screen h-screen relative flex pl-8 items-center overflow-hidden">
+      
+      {/* Foreground Elements */}
+      <div className="float-start z-99 translate-y-15">
+        <div className="absolute lg:-translate-y-45 -translate-y-55 translate-x-8 lg:translate-x-13">
+          <Image 
+            src={Logo}
+            alt="Logo"
+            height={400}
+            className='block not-lg:hidden'
+          />
+        </div>
+        <div className="absolute -translate-y-35 translate-x-16">
+          <Image 
+            src={Logo}
+            alt="Logo"
+            height={300}
+            className='block lg:hidden'
+          />
+        </div>
+        {/* Logo */}    
+
+        {/* Clickable transparent boxes */}
+        {isMenu && <div onClick={() => {
+          SetIsMenu(c => !c)
+        }} className='z-999 border-2 h-12 lg:h-14 absolute w-46 lg:w-56 translate-x-20 translate-y-29 lg:translate-x-24 lg:translate-y-35 bg-transparent cursor-pointer'></div>} 
+        {isMenu && <div className='z-999 border-2 h-12 lg:h-14 absolute w-45 lg:w-55 translate-x-20 lg:translate-x-24 translate-y-50 lg:translate-y-60 bg-transparent cursor-pointer'></div>} 
+        {isMenu && <div className='z-999 border-2 h-12 lg:h-14 absolute w-30 lg:w-36 translate-x-27 lg:translate-x-33 translate-y-70 lg:translate-y-84 bg-transparent cursor-pointer'></div>} 
+        {isMenu && <div className='z-999 border-2 h-10 lg:h-12 absolute w-15 lg:w-18 translate-x-35 lg:translate-x-42 translate-y-88 lg:translate-y-105 bg-transparent cursor-pointer'></div>}
+
+        {/* Menu images */}
+        {isMenu && <Image 
+          src={Menu}
+          alt="Menu"
+          height={600}
+          className="hidden lg:block"
+        />}
+        {isMenu && <Image 
+          src={Menu}
+          alt="Menu"
+          height={500}
+          className="block lg:hidden"
+        />}
+      </div>
+
+      {/* Background for Mobile */}
+      <div className="block lg:hidden absolute inset-0">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src={BackgroundImage}
+          alt="Warriors looking at a fort"
+          fill
+          style={{ objectFit: 'contain' }}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+      {/* Background for Desktop */}
+      <div className="hidden lg:block absolute inset-0">
+        <Image 
+          src={BackgroundImageDesktop}
+          alt="Warriors looking at a fort"
+          fill
+          style={{ objectFit: 'cover' }}
+          priority
+        />
+      </div>
     </div>
   );
 }
